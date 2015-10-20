388646b8db431c90c8d4836dfdea6f395f1ef0d9
Aleksandr Feer <pertik@list.ru>
2015-10-21 02:47:37 +0300

    fifth commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,84 @@
-var gulp = require("gulp"),
-    browserSync = require('browser-sync');
+/* ----------------plugins---------------- */
 
-// Watch
-gulp.task('watch', function() { // следить за изменениями в файлах
-	gulp.watch([
-		'app/*.html',
-		'app/*.php',
-		'app/js/**/*.js',
-		'app/css/**/*.css'
-	]).on('change', browserSync.reload);
+var
+	gulp        = require('gulp'),
+	compass     = require('gulp-compass'),
+	jade        = require('gulp-jade'),
+	browserSync = require('browser-sync').create(),
+	plumber     = require('gulp-plumber');
+
+/* ----------------paths---------------- */
+
+var
+	paths = {
+		jade : {
+			location: 'app/jade_pages/**/*.jade',
+			compiled: 'app/html_pages/*.jade',
+			destination: '.'
+		},
+
+		scss : {
+			location: 'app/css/scss/**/*.scss',
+			entryPoint: 'app/css/css/main.css'
+		},
+
+		compass: {
+			configFile: 'config.rb',
+			cssFolder: 'app/css/css/',
+			scssFolder: 'app/css/scss/',
+			imgFolder: 'app/img/'
+		},
+
+		browserSync: {
+			baseDir: './',
+			watchPaths: ['app/html_pages/*.html', 'app/css/css/*.css', 'app/js/*.js']
+		}
+
+	}
+
+/* ----------------jade---------------- */
+
+gulp.task('jade', function() {
+	gulp.src(paths.jade.compiled)
+		.pipe(plumber())
+		.pipe(jade({
+			pretty: '\t',
+		}))
+		.pipe(gulp.dest(paths.jade.destination));
 });
 
-gulp.task('default', ['watch']); // работает по умолчанию
+/* ----------------scss-compass---------------- */
+
+gulp.task('compass', function() {
+	gulp.src(paths.scss.location)
+		.pipe(plumber())
+		.pipe(compass({
+			config_file: paths.compass.configFile,
+			css: paths.compass.cssFolder,
+			sass: paths.compass.scssFolder,
+			image: paths.compass.imgFolder
+		}));
+});
+
+/* ----------------browser-sync---------------- */
+
+gulp.task('sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: paths.browserSync.baseDir
+		}
+	});
+});
+
+/* --------- watch --------- */
+
+gulp.task('watch', function(){
+	gulp.watch(paths.jade.location, ['jade']);
+	gulp.watch(paths.scss.location, ['compass']);
+	
+	gulp.watch(paths.browserSync.watchPaths).on('change', browserSync.reload);
+});
+
+/* --------- default --------- */
+
+gulp.task('default', ['jade', 'compass', 'sync', 'watch']);
